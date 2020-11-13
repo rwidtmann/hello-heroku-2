a78e8a9196aeaef50bff74a411bf231f622ce698
Richard Widtmann <rwidtmann@sbcglobal.net>
2020-11-13 15:19:12 -0600

    First Commit

---
+++ src/main/java/com/trilogyed/HelloHeroku2Application.java
@@ -0,0 +1,13 @@
+package com.trilogyed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHeroku2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHeroku2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/trilogyed/HelloHeroku2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHeroku2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
